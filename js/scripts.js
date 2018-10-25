a2d6d5b7fe2883609c861ef0cbf57ecf01395fea
Cristian Lucero and Manasa Vesala <manasa@>
2018-10-25 13:14:00 -0700

    roman numerals added if

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@ var toRoman = function(number){
 var integer = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 var result = " ";
+    if ( number > 3999 || number===0) {
 
+        return "invalid number";
+    }
     for (var i =0; i<=integer.length; i++ ){
 
 
